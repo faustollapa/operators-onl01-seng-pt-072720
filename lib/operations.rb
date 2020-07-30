9ae76e02730ca829f4d26c23d2c17e553d7c7ae9
Fausto Llapa <faustollapa850@gmail.com>
2020-07-30 18:43:55 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,12 @@
 def unsafe?(speed)
-  if speed > 60 
-     true
-  elsif speed < 40
-     true
-  else
-     false
-  end    
+  	if speed > 60 
+		true
+	elsif 
+		speed < 40
+		true
+	else
+		false
+	end
 end
 
 
